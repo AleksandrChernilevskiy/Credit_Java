a133a933efcaab3ccdca01aaf1bebef62ff01fa3
Azez <aleksanderterar@gmail.com>
2023-01-26 22:10:10 +0300

    change

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public double calculate( int year) {
-        double interestRate = 9.99; //Процентная ставка
-        int balanceOfTheLoan = 1_000_000; //Остаток суммы кредита
+    public double calculate( int year, int balanceOfTheLoan, double interestRate) {
+        //double interestRate = 9.99; //Процентная ставка
+        //int balanceOfTheLoan = 1_000_000; //Остаток суммы кредита
         int interestPeriods = year * 12; //процентные периоды до окончания срока кредита (в месяцах)
         double monthlyInterestRate = interestRate / ( 100 * 12 ); //месячная процентная ставка (рассчитывается как ставка по кредиту /100 *12)
         double result;
