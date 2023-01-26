d3d701fe6f3c508f09fb353e2eacd5a237b6d21b
Azez <aleksanderterar@gmail.com>
2023-01-26 22:37:20 +0300

    Refactor

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,12 @@
 public class CreditPaymentService {
-    public double calculate( int year, int balanceOfTheLoan, double interestRate) {
-        //double interestRate = 9.99; //Процентная ставка
-        //int balanceOfTheLoan = 1_000_000; //Остаток суммы кредита
+    public double calculate(int year, int balanceOfTheLoan, double interestRate) {
+        //double interestRate - Процентная ставка
+        //int balanceOfTheLoan - Остаток суммы кредита
         int interestPeriods = year * 12; //процентные периоды до окончания срока кредита (в месяцах)
-        double monthlyInterestRate = interestRate / ( 100 * 12 ); //месячная процентная ставка (рассчитывается как ставка по кредиту /100 *12)
+        double monthlyInterestRate = interestRate / (100 * 12); //месячная процентная ставка (рассчитывается как ставка по кредиту /100 *12)
         double result;
-        if ( interestPeriods >= 12) {
-            result = balanceOfTheLoan * ( monthlyInterestRate / ( 1 - Math.pow( (1 + monthlyInterestRate), -interestPeriods) ));
+        if (interestPeriods >= 12) {
+            result = balanceOfTheLoan * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), -interestPeriods)));
         } else {
             result = 0;
         }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int year;
-        double monthlyPayment = service.calculate(2, 1_000_000, 9.99 );
+        double monthlyPayment = service.calculate(2, 1_000_000, 9.99);
         System.out.println("Кредит 1.000.000 рублей");
         System.out.println("Процентная ставка 9.99% годовых");
         System.out.println("Ваш ежемесячный платёж по кредиту " + (int) monthlyPayment + " рублей в месяц.");
